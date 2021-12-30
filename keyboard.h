54b9d5617991218ad74c620c50dba0fc367ce472
dylanbobb <dylan.bobb@hotmail.com>
2021-12-30 02:55:06 -0500

    Start writing KeyboardEventHandler

--- keyboard.h
+++ keyboard.h
@@ -6,12 +6,22 @@
     #include "driver.h"
     #include "port.h"
 
+    class KeyboardEventHandler
+    {
+        KeyboardEventHandler();
+
+        virtual void onKeyDown();
+        virtual void onKeyUp();
+    };
+
     class KeyboardDriver : public InterruptHandler, public Driver
     {
         Port8Bit dataPort;
         Port8Bit commandPort;
+
+        KeyboardEventHandler* handler;
         public:
-            KeyboardDriver(InterruptManager* manager);
+            KeyboardDriver(InterruptManager* manager, KeyboardEventHandler* handler);
             ~KeyboardDriver();
             virtual uint32_t handleInterrupt(uint32_t esp);
             virtual void activate();
